179c29044a1b0307b978edf39a521232e56fafea
adel <adelsadeq24@gmail.com>
2022-10-03 01:49:25 +0200

    adding some ui fields

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,33 @@
 import React from 'react'
 // CSS Imports
 import '../styles/global.css'
-import '../styles/login.css'
+import '../styles/Login/login.css'
+import { Link } from "react-router-dom";
+import radicalXLogo from '../assets/RadicallX-Black-Logo 1.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div class="split left">
+      </div>
+
+      <div class="split right">
+        <img src={radicalXLogo} className="radicalX-logo"></img>
+        <form class="input-fields">
+          <h1>Login</h1>
+          <input type="email" id="email" name="email" placeholder="Email" required /><br />
+          <input type="password" id="password" name="password" placeholder="Password" required /><br />
+          <div>
+            <label className='remember-me'> 
+              <input type="checkbox" id="remember" name="remember" value="remember" />Remember me
+            </label>
+            <a href="/some link ...." className='forget-password'>Forgot password? </a>
+          </div>
+          <button className='login-button'>Login</button>
+        </form>
+      </div>
+    </div>
+
   )
 }
 
