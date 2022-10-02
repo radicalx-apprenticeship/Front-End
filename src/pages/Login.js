4611fc7d851fbfa5c1adf8977236cd1f6f1da99e
h-sompura <sompharshi123@gmail.com>
2022-10-02 14:42:27 -0400

    2 Column Login Page Setup Completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,20 @@
 // Add all Login components here in this file
 import React from "react";
 // CSS Imports
-import "../styles/global.css";
 import "../styles/login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return(
+    <div className="main-container">
+      <div className="left-container">
+      {/* Purple left container */}
+      </div>
+      <div className="right-container">
+      {/* Login Form/Right Container/Component goes here*/}
+      Login
+      </div>
+    </div>
+  );
 };
 
 export default Login;
