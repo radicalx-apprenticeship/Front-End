c0d7e7520c41f89dc968a79683dbe19cd098feb4
Mohammed Sameer <samaug198@gmail.com>
2022-10-03 18:51:03 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/Navigation/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import ShowAndHidePassword from "./show-and-hide-password/ShowAndHidePassword";
+
 
 function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          {/* Why private route? - so only signed users can access this page!(See PrivateRoute.js for more info) */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Dashboard />} />
-            {/* TODO: Add other private routes here */}
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+  render(){
+    return (
+      <ShowAndHidePassword />
+      <Router>
+        <AuthProvider>
+          <Routes>
+            {/* Why private route? - so only signed users can access this page!(See PrivateRoute.js for more info) */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Dashboard />} />
+              {/* TODO: Add other private routes here */}
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+
+    );
+  }
 }
 
 export default App;
