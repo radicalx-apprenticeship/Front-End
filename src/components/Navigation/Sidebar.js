a53c60c360ce69b59dcb3741aeb691d01a6bea48
h-sompura <sompharshi123@gmail.com>
2022-10-14 19:01:10 -0400

    Prettier run

--- src/components/Navigation/Sidebar.js
+++ src/components/Navigation/Sidebar.js
@@ -4,7 +4,7 @@ import "../../styles/Dashboard/sidebar.css";
 import radicalXLogo from "../../assets/RadicallX-Black-Logo 1.png";
 import dashboard from "../../assets/dashboard-3.svg";
 import apprencticeship from "../../assets/medal-star.svg";
-import book from '../../assets/book.svg';
+import book from "../../assets/book.svg";
 import briefcase from "../../assets/briefcase.svg";
 import settings from "../../assets/setting-2.svg";
 import avatar from "../../assets/avatar.jpeg";
@@ -13,30 +13,30 @@ const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <img src={radicalXLogo} className="sidebar-logo" alt="RadicalX" />
-      <br/>
+      <br />
       <div className="nav-link">
-        <img className="nav-icon" src={dashboard}/>
+        <img className="nav-icon" src={dashboard} />
         <p className="link-text">Dashboard</p>
       </div>
       <div className="nav-link active-link">
-        <img className="nav-icon" src={apprencticeship}/>
+        <img className="nav-icon" src={apprencticeship} />
         <p className="link-text">Apprenticeships</p>
       </div>
       <div className="nav-link">
-        <img className="nav-icon" src={book}/>
+        <img className="nav-icon" src={book} />
         <p className="link-text">Internships</p>
       </div>
       <div className="nav-link">
-        <img className="nav-icon" src={briefcase}/>
+        <img className="nav-icon" src={briefcase} />
         <p className="link-text">Jobs</p>
       </div>
       <div className="nav-link">
-        <img className="nav-icon" src={settings}/>
+        <img className="nav-icon" src={settings} />
         <p className="link-text">Settings</p>
       </div>
       {/* Footer/User */}
       <div className="sidebar-footer">
-        <img className="avatar" src={avatar}/>
+        <img className="avatar" src={avatar} />
         <p>Adam Scott</p>
       </div>
     </div>
