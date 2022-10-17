8e83e83bf2bb602eb5f95cbbb2c78c82a188eff1
h-sompura <sompharshi123@gmail.com>
2022-10-17 13:38:37 -0400

    Updating Cards and styling

--- src/components/Dashboard/Card.js
+++ src/components/Dashboard/Card.js
@@ -1,28 +1,38 @@
 import "../../styles//Dashboard/Card.css";
+import CardButtons from "./CardButtons";
 
 function Card() {
   const posts = [
     {
       id: 1,
-      title: "Tokka",
-      body: "Tokka is drinking lemon juice",
+      title: "Mobile App Design",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
     },
     {
       id: "2",
-      title: "Adel",
-      body: "Adel is Eating Apples",
+      title: "Web Development",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
     },
     {
       id: "3",
-      title: "Adel",
-      body: "Adel is Eating Apples",
+      title: "Full Stack Development",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
+    },
+    {
+      id: "4",
+      title: "Growth Management",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
     },
   ];
   return (
     <div className="cards-content">
       {posts.map((post) => (
         <div key={post.id} className="each">
-          <h3 className="card-title">{post.title}</h3>
+          <div className="card-header">
+            {/* Card Header */}
+            <h3 className="card-title">{post.title}</h3>
+            <CardButtons />
+          </div>
           <p className="card-body">{post.body}</p>
         </div>
       ))}
