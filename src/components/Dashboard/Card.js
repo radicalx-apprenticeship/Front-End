3801cf75acfaa81cbc76fc3ff3b05b4e3b161185
h-sompura <sompharshi123@gmail.com>
2022-10-17 13:11:19 -0400

    Prettier run and some formatting

--- src/components/Dashboard/Card.js
+++ src/components/Dashboard/Card.js
@@ -1,35 +1,33 @@
-import '../../styles//Dashboard/Card.css'
-
-
-
+import "../../styles//Dashboard/Card.css";
 
 function Card() {
-    const posts = [
-        {
-            id: 1,
-            "title": "Tokka",
-            "body": "Tokka is drinking lemon juice",
-        },
-        {
-            id: "2",
-            "title": "Adel",
-            "body": "Adel is Eating Apples"
-        },
-        {
-            id: "3",
-            "title": "Adel",
-            "body": "Adel is Eating Apples"
-        }
-    ]
-    return (
-        <div className='cards-content'>
-            {posts.map(post => (
-                <div key={post.id} className="each">
-                    <h3 className='card-title'>{post.title}</h3>
-                    <p className="card-body">{post.body}</p>
-                </div>))}
+  const posts = [
+    {
+      id: 1,
+      title: "Tokka",
+      body: "Tokka is drinking lemon juice",
+    },
+    {
+      id: "2",
+      title: "Adel",
+      body: "Adel is Eating Apples",
+    },
+    {
+      id: "3",
+      title: "Adel",
+      body: "Adel is Eating Apples",
+    },
+  ];
+  return (
+    <div className="cards-content">
+      {posts.map((post) => (
+        <div key={post.id} className="each">
+          <h3 className="card-title">{post.title}</h3>
+          <p className="card-body">{post.body}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
