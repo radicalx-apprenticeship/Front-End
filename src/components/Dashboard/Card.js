dcd6fe5742d20375e647865973e41e31110016c0
Harshitha Sompura <sompharshi123@gmail.com>
2022-10-17 13:59:24 -0400

    Cards UI Complete (#17)
    * cards are added
    
    * Layout-fixed
    
    * Reverted firebase-keys.js to original
    
    * Prettier run and some formatting
    
    * Adding card assets as svgs and removing pngs
    
    * Updating card icons in CardButton.js
    
    * Updating Cards and styling
    
    * Created Card tags
    
    Co-authored-by: adel <adelsadeq24@gmail.com>

---
+++ src/components/Dashboard/Card.js
@@ -0,0 +1,53 @@
+import "../../styles//Dashboard/Card.css";
+import CardButtons from "./CardButtons";
+
+function Card() {
+  const posts = [
+    {
+      id: 1,
+      title: "Mobile App Design",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
+      tags: ["Product Manager", "Frontend Developer", "Backend Developer"],
+    },
+    {
+      id: "2",
+      title: "Web Development",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
+      tags: ["Product Manager", "Frontend Developer", "Backend Developer"],
+    },
+    {
+      id: "3",
+      title: "Full Stack Development",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
+      tags: ["Product Manager", "Frontend Developer", "Backend Developer"],
+    },
+    {
+      id: "4",
+      title: "Growth Management",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua.",
+      tags: ["Product Manager", "Frontend Developer", "Backend Developer"],
+    },
+  ];
+  return (
+    <div className="cards-content">
+      {posts.map((post) => (
+        <div key={post.id} className="each">
+          <div className="card-header">
+            {/* Card Header */}
+            <h3 className="card-title">{post.title}</h3>
+            <CardButtons />
+          </div>
+          <p className="card-body">{post.body}</p>
+          <div className="card-keywords">
+            {/* Apprenticeships tags/keywords */}
+            {/* {post.tags.map((tag) => <p>{tag}</p>)} */}
+            {/* <p>{post.tags}</p> */}
+            <p>{post.tags}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Card;
