922699389fc8837ce518dc67553b9abe661a74cb
h-sompura <sompharshi123@gmail.com>
2022-10-17 13:10:56 -0400

    Reverted firebase-keys.js to original

--- src/config/firebase-keys.js
+++ src/config/firebase-keys.js
@@ -1,16 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDioI8JcGbdQZUL9vgYy1tR37bgtNW_nAU",
-
-  authDomain: "radicalx-auth.firebaseapp.com",
-
-  projectId: "radicalx-auth",
-
-  storageBucket: "radicalx-auth.appspot.com",
-
-  messagingSenderId: "1052810847197",
-
-  appId: "1:1052810847197:web:0504482ea392b2b833cc34"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
