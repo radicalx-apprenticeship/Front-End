69b0e0437b9059b0064777bd820b308767b6c358
adel <adelsadeq24@gmail.com>
2022-10-17 18:52:29 +0200

    cards are added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "../components/Navigation/Sidebar";
 import AddLogo from "../assets/add-square.svg";
+import Card from "../components/Dashboard/Card";
 import { Button } from "../components";
 // CSS Imports
 import "../styles/Dashboard/dashboard.css";
@@ -9,11 +10,14 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <Sidebar />
-      <div className="page-header">
-        <h1>Apprenticeship</h1>
-        <Button icon={AddLogo} href="#">
-          Create New Apprenticeship
-        </Button>
+      <div className="main-content">
+        <div className="page-header">
+          <h1>Apprenticeship</h1>
+          <Button icon={AddLogo} href="#">
+            Create New Apprenticeship
+          </Button>
+          <Card />
+        </div>
       </div>
     </div>
   );
