608c24ffabc492fec2e3132d6365c952e527d175
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-11-14 17:09:40 +0200

    fix: add video list to the intro card

--- src/pages/Apprenticeship/IntroVid.jsx
+++ src/pages/Apprenticeship/IntroVid.jsx
@@ -1,27 +1,38 @@
 import { useState } from "react";
 import { CardContainer } from "../../components";
 import DocumentUpload from "../../assets/document-upload.svg";
+import Close from "../../assets/close.svg";
 const IntroVid = () => {
-  const [video, setVideo] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const onDelete = (index) => {
+    const newVideos = videos.filter((_, i) => i !== index);
+    setVideos(newVideos);
+  };
   return (
     <CardContainer title="Introduce yourself, your company, and what you're building.">
       <label htmlFor="video" className="video-container">
-        {video ? (
-          <div>{video.name}</div>
-        ) : (
-          <>
-            <p>Drag n drop to upload your video</p>
-            <img src={DocumentUpload} alt="document upload" />
-          </>
-        )}
+        <p>Drag n drop to upload your video</p>
+        <img src={DocumentUpload} alt="document upload" />
 
         <input
           type="file"
           id="video"
           accept="video/*"
-          onChange={(e) => setVideo(e.target.files[0])}
+          onChange={(e) => setVideos((prev) => [...prev, e.target.files[0]])}
         />
       </label>
+      <div className="video-button-container">
+        {videos.map((video, index) => (
+          <button
+            key={index}
+            onClick={() => onDelete(index)}
+            className="video-button"
+          >
+            <p>{video.name}</p>
+            <img src={Close} alt="close" />
+          </button>
+        ))}
+      </div>
     </CardContainer>
   );
 };
