f14ac7449c831bc559b1dbba94e46c3a03e5c786
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-11-14 16:54:55 +0200

    fix: Change the card container to accept a header button

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,10 +1,13 @@
 import styles from "./CardContainer.module.css";
 import infoIcon from "../../assets/info.svg";
-const CardContainer = ({ title, children }) => {
+const CardContainer = ({ title, children, headerButton }) => {
   return (
     <div className={styles.container}>
       <div className={styles.container__header}>
-        <p>{title}</p>
+        <div className={styles.container__titleContainer}>
+          <p>{title}</p>
+          {headerButton}
+        </div>
         <img src={infoIcon} alt="info" />
       </div>
       {children}
